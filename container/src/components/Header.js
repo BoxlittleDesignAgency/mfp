757e39853ec5af9f9023540ac73de9ba4cf0eece
Demola Adebayo <ademoladebayo@gmail.com>
2021-06-26 15:16:15 +0100

    Added console log to Header

--- container/src/components/Header.js
+++ container/src/components/Header.js
@@ -56,6 +56,7 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Header({ signedIn, onSignOut }) {
   const classes = useStyles();
+  console.log("yesss this works!!")
 
   const onClick = () => {
     if (signedIn && onSignOut) {
